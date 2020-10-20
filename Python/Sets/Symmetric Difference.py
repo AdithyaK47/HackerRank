633b17fcbdabd98e0a890f9d2426d32be3f3065f
Adithya Krishna <68055622+AdithyaK47@users.noreply.github.com>
2020-10-20 19:52:21 +0530

    Update Symmetric Difference.py

--- Python/Sets/Symmetric Difference.py
+++ Python/Sets/Symmetric Difference.py
@@ -3,8 +3,7 @@ set1 = set(input().split())
 n2 = int(input())
 set2 = set(input().split())
 
-d1 = set2.difference(set1)
-d2 = set1.difference(set2)
-differ = d1.union(d2)
+differ = sorted(set1.symmetric_difference(set2))
 
-print ('\n'.join(sorted(differ, key=int)))
+for i in differ:
+  print(i)
